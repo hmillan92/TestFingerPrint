2254edc1718a30a8863b42d5beb0da5f410d0914
hmillan92 <45247533+hmillan92@users.noreply.github.com>
2019-12-17 12:40:23 -0400

    Proyectos del SDK Ejecutandose correctamente

---
+++ Test FingerPrint/FormFingerPrint/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormFingerPrint
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            fmrRegistrar FormRegistrar = new fmrRegistrar();
+            FormRegistrar.ShowDialog();
+            
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            fmrVerificar FormVerificar = new fmrVerificar();
+            FormVerificar.ShowDialog();
+        }
+    }
+}
